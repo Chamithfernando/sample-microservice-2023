412e01f3bacf0c02f84e2ca20d49ecdf3c9a46de
chamithf <chameeefdo@gmail.com>
2023-09-30 21:07:24 +0530

    initial commit

---
+++ src/test/java/com/chamith/samplemicroservice/SampleMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chamith.samplemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
